5d5f50fc476ab255e7d3ca21a929d462b159a935
Glitch (glitch-hello-website) <none>
2025-06-09 05:58:57 +0000

    🔦📍 Checkpoint ./server.js:78855186/2800

--- server.js
+++ server.js
@@ -2,20 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static('.'));
+
 app.get('/', (req, res) => {
-  console.log('GET / accessed');
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-app.get(['/const', '/const/'], (req, res) => {
-  console.log('GET /const accessed');
-  res.sendFile(path.join(__dirname, 'constructing.html'));
-});
-
-app.get('/temp.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'temp.html'));
-});
-
 
 const listener = app.listen(process.env.PORT, () => {
   console.log('Listening on port ' + listener.address().port);

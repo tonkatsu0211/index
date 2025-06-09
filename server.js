dfa6df0590c67ac3f67d9aa55ea624b5ab9546a1
Glitch (glitch-hello-website) <none>
2025-06-09 08:57:31 +0000

    🎳🕍 Checkpoint ./server.js:78855186/1612 ./error.html:78855186/1147 ./popular.html:78855186/557

--- server.js
+++ server.js
@@ -8,14 +8,23 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/constructing.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'constructing.html'));
-});
-
-app.get(['/projects.html','popular.html'], (req, res) => {
+app.get('/projects', (req, res) => {
   res.sendFile(path.join(__dirname, 'projects.html'));
 });
 
+app.get('/constructing', (req, res) => {
+  res.sendFile(path.join(__dirname, 'constructing.html'));
+});
+
+app.get('/:page/', (req, res) => {
+  const pageName = req.params.page;
+  res.redirect(`/error.html?e=${encodeURIComponent(pageName)}`);
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
 const listener = app.listen(process.env.PORT, () => {
-  console.log('Listening on port ' + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });

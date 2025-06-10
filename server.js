68e96aed52be29edc12b53bdd3296ad28ca2f7bb
Glitch (glitch-hello-website) <none>
2025-06-10 00:29:55 +0000

    🏢🌇 Checkpoint ./server.js:78855186/217 ./lobby.html:78855186/1182 ./index.html:78855186/51 ./popular.html:78855186/42 ./projects.html:78855186/42 ./updates.html:78855186/36 ./contact.html:78855186/36 ./constructing1.html:78855186/75 ./constructing.html:78855186/7 ./style.css:78855186/640 ./footer.html:78855186/39

--- server.js
+++ server.js
@@ -20,16 +20,20 @@ app.get('/constructing1', (req, res) => {
   res.sendFile(path.join(__dirname, 'constructing1.html'));
 });
 
-app.get('/popular.html', (req, res) => {
-  res.redirect('popular.html');
+app.get('/popular', (req, res) => {
+  res.sendFile(path.join(__dirname, 'popular.html'));
 });
 
 app.get('/contact', (req, res) => {
   res.sendFile(path.join(__dirname, 'contact.html'));
 })
 
-app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'contact.html'));
+app.get('/lobby', (req, res) => {
+  res.sendFile(path.join(__dirname, 'lobby.html'));
+})
+
+app.get('/deleteHistory', (req, res) => {
+  res.sendFile(path.join(__dirname, 'lobby.html'));
 })
 
 app.get((req, res) => {

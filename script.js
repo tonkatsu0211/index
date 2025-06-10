f42ec64122576339fcae6d5937ea24d1343c99c0
Glitch (glitch-hello-website) <none>
2025-06-10 06:45:29 +0000

    🍯🕌 Checkpoint ./public/constructing.html:78855186/2446 ./public/constructing1.html:78855186/2446 ./public/index.html:78855186/4059 ./public/contact.html:78855186/3001 ./public/breakHistory.html:78855186/3548 ./public/beforeBreak.html:78855186/3067 ./public/error.html:78855186/4353 ./public/my.html:78855186/4195 ./public/projects.html:78855186/4642 ./public/style.css:78855186/9645 ./public/updates.html:78855186/2609 ./script.js:78855186/2237 ./server.js:78855186/2979

--- script.js
+++ script.js
@@ -11,24 +11,27 @@ Make the "Click me!" button move when the visitor clicks it:
 - First add the button to the page by following the steps in the TODO 🚧
 */
 const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
+if (btn) {
+  // Detect clicks on the button
   btn.onclick = function () {
     // The 'dipped' class in style.css changes the appearance on click
     btn.classList.toggle("dipped");
   };
 }
 
-
 // ----- GLITCH STARTER PROJECT HELPER CODE -----
 
 // Open file when the link in the preview is clicked
 let goto = (file, line) => {
   window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
+    { type: "glitch/go-to-line", payload: { filePath: file, line: line } },
+    "*"
   );
 };
 // Get the file opening button from its class name
 const filer = document.querySelectorAll(".fileopener");
 filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+  f.onclick = () => {
+    goto(f.dataset.file, f.dataset.line);
+  };
 });
--- server.js
+++ server.js
@@ -1,52 +1,52 @@
 "use strict";
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 app.use(express.static("public"));
 
-app.get(['/' , '/index' , '/index/'], (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get(["/", "/index", "/index/", "/top", "/top/"], (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get(['/my' , '/my/'], (req, res) => {
-  res.sendFile(path.join(__dirname, 'my.html'));
+app.get(["/my", "/my/"], (req, res) => {
+  res.sendFile(path.join(__dirname, "my.html"));
 });
 
-app.get(['/projects' , '/projects/'], (req, res) => {
-  res.sendFile(path.join(__dirname, 'projects.html'));
+app.get(["/projects", "/projects/"], (req, res) => {
+  res.sendFile(path.join(__dirname, "projects.html"));
 });
 
-app.get(['/constructing' , '/constructing/'], (req, res) => {
-  res.sendFile(path.join(__dirname, 'constructing.html'));
+app.get(["/constructing", "/constructing/"], (req, res) => {
+  res.sendFile(path.join(__dirname, "constructing.html"));
 });
 
-app.get(['/constructing1' , '/constructing1/'], (req, res) => {
-  res.sendFile(path.join(__dirname, 'constructing1.html'));
+app.get(["/constructing1", "/constructing1/"], (req, res) => {
+  res.sendFile(path.join(__dirname, "constructing1.html"));
 });
 
-app.get(['/contact' , '/contact/'], (req, res) => {
-  res.sendFile(path.join(__dirname, 'contact.html'));
+app.get(["/contact", "/contact/"], (req, res) => {
+  res.sendFile(path.join(__dirname, "contact.html"));
 });
 
-app.get(['/lobby' , '/lobby/'], (req, res) => {
-  res.sendFile(path.join(__dirname, 'lobby.html'));
+app.get(["/lobby", "/lobby/"], (req, res) => {
+  res.sendFile(path.join(__dirname, "lobby.html"));
 });
 
-app.get(['/breakHistory' , '/breakHistory/'], (req, res) => {
-  res.sendFile(path.join(__dirname, 'breakHistory.html'));
+app.get(["/breakHistory", "/breakHistory/"], (req, res) => {
+  res.sendFile(path.join(__dirname, "breakHistory.html"));
 });
 
-app.get(['/error' , '/error/'], (req, res) => {
-  res.sendFile(path.join(__dirname, 'error.html'));
+app.get(["/error", "/error/"], (req, res) => {
+  res.sendFile(path.join(__dirname, "error.html"));
 });
 
 app.use((req, res) => {
-  const pageName = req.path.replace('/', '');
-  console.log('Fallback 404 for:', req.path);
+  const pageName = req.path.replace("/", "");
+  console.log("Fallback 404 for:", req.path);
   res.status(404).redirect(`/error.html?e=${encodeURIComponent(pageName)}`);
 });
 
 const listener = app.listen(process.env.PORT, () => {
-  console.log('App listening on port ' + listener.address().port);
+  console.log("App listening on port " + listener.address().port);
 });

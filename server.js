a578684e61567378e721c8c8e9496246bbe049cd
Glitch (glitch-hello-website) <none>
2025-06-11 01:36:19 +0000

    🏊🏼🍖 Checkpoint ./server.js:78855186/145 ./public/index.html:78855186/60 ./public/my.html:78855186/35 ./public/projects.html:78855186/5873 ./public/updates.html:78855186/3519 ./public/error.html:78855186/40 ./public/contact.html:78855186/35 ./public/constructing1.html:78855186/35 ./public/constructing.html:78855186/35 ./public/beforeBreak.html:78855186/50

--- server.js
+++ server.js
@@ -6,45 +6,44 @@ const path = require("path");
 app.use(express.static("public"));
 
 app.get(["/", "/index", "/index/", "/top", "/top/"], (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get(["/my", "/my/"], (req, res) => {
-  res.sendFile(path.join(__dirname, "my.html"));
+  res.sendFile(path.join(__dirname, "public", "my.html"));
 });
 
 app.get(["/projects", "/projects/"], (req, res) => {
-  res.sendFile(path.join(__dirname, "projects.html"));
+  res.sendFile(path.join(__dirname, "public", "projects.html"));
 });
 
 app.get(["/constructing", "/constructing/"], (req, res) => {
-  res.sendFile(path.join(__dirname, "constructing.html"));
+  res.sendFile(path.join(__dirname, "public", "constructing.html"));
 });
 
 app.get(["/constructing1", "/constructing1/"], (req, res) => {
-  res.sendFile(path.join(__dirname, "constructing1.html"));
+  res.sendFile(path.join(__dirname, "public", "constructing1.html"));
 });
 
 app.get(["/contact", "/contact/"], (req, res) => {
-  res.sendFile(path.join(__dirname, "contact.html"));
+  res.sendFile(path.join(__dirname, "public", "contact.html"));
 });
 
 app.get(["/beforeBreak", "/beforeBreak/"], (req, res) => {
-  res.sendFile(path.join(__dirname, "beforeBreak.html"));
+  res.sendFile(path.join(__dirname, "public", "beforeBreak.html"));
 });
 
 app.get(["/breakHistory", "/breakHistory/"], (req, res) => {
-  res.sendFile(path.join(__dirname, "breakHistory.html"));
+  res.sendFile(path.join(__dirname, "public", "breakHistory.html"));
 });
 
 app.get(["/error", "/error/"], (req, res) => {
-  res.sendFile(path.join(__dirname, "error.html"));
+  res.sendFile(path.join(__dirname, "public", "error.html"));
 });
 
 app.use((req, res) => {
   const pageName = req.path.replace("/", "");
-  console.log("Fallback 404 for:", req.path);
-  res.status(404).redirect(`/error.html?e=${encodeURIComponent(pageName)}`);
+  res.status(404).redirect(`/error?e=${encodeURIComponent(pageName)}`);
 });
 
 const listener = app.listen(process.env.PORT, () => {

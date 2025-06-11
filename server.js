a2e101d93eaf843c12c298300def2c64bf74893b
Glitch (glitch-hello-website) <none>
2025-06-11 05:14:45 +0000

    📇😋 Checkpoint ./server.js:78855186/260 ./views/my.ejs:78855186/1151 ./views/projects.ejs:78855186/920 ./views/updates.ejs:78855186/932 ./views/error.ejs:78855186/1920 ./views/style.css:78855186/38 ./views/contact.ejs:78855186/910 ./views/constructing1.ejs:78855186/906 ./views/constructing.ejs:78855186/906 ./views/beforeBreak.ejs:78855186/913

--- server.js
+++ server.js
@@ -3,43 +3,45 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static("views"));
+
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'public'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.get(["/", "/index", "/index/", "/top", "/top/"], (req, res) => {
-  res.render("index.html", { title: "_tonkatsu_のページ"});
+  res.render("index", { title: "_tonkatsu_のページ"});
 });
 
 app.get(["/my", "/my/"], (req, res) => {
-  res.render("my.html", { title: "_tonkatsu_の自己紹介(事故紹介)"});
+  res.render("my", { title: "_tonkatsu_の自己紹介(事故紹介)"});
 });
 
 app.get(["/projects", "/projects/"], (req, res) => {
-  res.render("projects.html", { title: "tonkatsu0211の作品"});
+  res.render("projects", { title: "tonkatsu0211の作品"});
 });
 
 app.get(["/constructing", "/constructing/"], (req, res) => {
-  res.render("constructing.html", { title: "建設中のページ"});
+  res.render("constructing", { title: "建設中のページ"});
 });
 
 app.get(["/constructing1", "/constructing1/"], (req, res) => {
-  res.render("constructing1.html", { title: "建設中のページ"});
+  res.render("constructing1", { title: "建設中のページ"});
 });
 
 app.get(["/contact", "/contact/"], (req, res) => {
-  res.render("contact.html", { title: "_tonkatsu_への連絡先"});
+  res.render("contact", { title: "_tonkatsu_への連絡先"});
 });
 
 app.get(["/beforeBreak", "/beforeBreak/"], (req, res) => {
-  res.render("beforeBreak.html", { title: "_tonkatsu_のページ"});
+  res.render("beforeBreak", { title: "_tonkatsu_のページ"});
 });
 
 app.get(["/updates", "/updates/"], (req, res) => {
-  res.render("updates.html", { title: "ページ更新履歴"});
+  res.render("updates", { title: "ページ更新履歴"});
 });
 
 app.get(["/error", "/error/"], (req, res) => {
-  res.render("error.html", { title: "404 Not Found"});
+  res.render("error", { title: "404 Not Found"});
 });
 
 app.use((req, res) => {
@@ -50,12 +52,4 @@ app.use((req, res) => {
 const port = process.env.PORT || 3000;
 const listener = app.listen(process.env.PORT, () => {
   console.log("App listening on port " + listener.address().port);
-});
-
-app.get('/', (req, res) => {
-  res.render('index', { title: 'ホーム' });
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact', { title: '連絡先' });
 });

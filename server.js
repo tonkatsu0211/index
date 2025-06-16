e9ddf334b7cfa48663f55363bdd9af151704cbfb
Glitch (glitch-hello-website) <none>
2025-06-16 10:33:20 +0000

    🏪💧 Checkpoint ./server.js:78855186/602

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cookieParser = require("cookie-parser");
+var e = "none";
 
 app.use(cookieParser());
 
@@ -11,14 +12,24 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public'));
 
-function render(res, view, data = {}) {
-    console.log(`access to /${view}`)
+
+function render(req, res, view, data = {}) {
+    const qE = req.query.e || "";
+    if (e != "none") {
+      console.log(`redirect by 404 to /error?e=${view}`)
+    } else {
+      console.log(`access to /${view}`)
+    }
+    if (view == "error" && qE){
+      console.log(`redirect by 404 to /error?e=${qE}`)
+    }
     res.render(view, data, (err, html) => {
       if (err) {
-        const e = 
+        var e = view
         console.log(`404 in /${view}`)
         res.status(404).redirect(`/error?e=${view}`); 
       } else {
+        var e = "none";
         res.send(html);
       }
     });
@@ -26,59 +37,59 @@ function render(res, view, data = {}) {
 
 app.get(["/", "/index", "/top", "/index.html"], (req, res) => {
   const from = req.query.f || "";
-  render(res, "index", { from, title: "_tonkatsu_のページ", page: "index", top: "_tonkatsu_ / tonkatsu0211のページにようこそ!!"});
+  render(req, res, "index", { from, title: "_tonkatsu_のページ", page: "index", top: "_tonkatsu_ / tonkatsu0211のページにようこそ!!"});
 });
 
 app.get(["/my", "/my.html"], (req, res) => {
-  render(res, "my", { title: "自己紹介(事故紹介)", page: "my", top: "自己紹介(事故紹介)"});
+  render(req, res, "my", { title: "自己紹介(事故紹介)", page: "my", top: "自己紹介(事故紹介)"});
 });
 
 app.get(["/projects", "/projects.html"], (req, res) => {
-  render(res, "projects", { title: "作品", page: "projects", top: "Scratchの作品"});
+  render(req, res, "projects", { title: "作品", page: "projects", top: "Scratchの作品"});
 });
 
 app.get("/const", (req, res) => {
-  render(res, "constructing", { title: "建設中のページ", page: "const", top: "建設中"});
+  render(req, res, "constructing", { title: "建設中のページ", page: "const", top: "建設中"});
 });
 
 app.get(["/constructing", "/const", "constructing.html"], (req, res) => {
-  render(res, "constructing", { title: "建設中のページ", page: "constructing", top: "建設中"});
+  render(req, res, "constructing", { title: "建設中のページ", page: "constructing", top: "建設中"});
 });
 
 app.get("/const1", (req, res) => {
-  render(res, "constructing", { title: "建設中のページ", page: "const1", top: "建設中"});
+  render(req, res, "constructing", { title: "建設中のページ", page: "const1", top: "建設中"});
 });
 
 app.get(["/constructing1", "/const1", "constructing1.html"], (req, res) => {
-  render(res, "constructing1", { title: "建設中のページ", page: "constructing1", top: "建設中"});
+  render(req, res, "constructing1", { title: "建設中のページ", page: "constructing1", top: "建設中"});
 });
 
 app.get(["/contact", "/contact.html"], (req, res) => {
-  render(res, "contact", { title: "お問い合わせ", page: "contact", top: "お問い合わせ"});
+  render(req, res, "contact", { title: "お問い合わせ", page: "contact", top: "お問い合わせ"});
 });
 
 app.get(["/beforeBreak", "/beforeBreak.html"], (req, res) => {
-  render(res, "beforeBreak", { title: "_tonkatsu_のページ", page: "beforeBreak", top: "履歴破壊"});
+  render(req, res, "beforeBreak", { title: "_tonkatsu_のページ", page: "beforeBreak", top: "履歴破壊"});
 });
 
 app.get(["/updates", "/updates.html"], (req, res) => {
-  render(res, "updates", { title: "ページ更新履歴", page: "updates", top: "ページ更新履歴"});
+  render(req, res, "updates", { title: "ページ更新履歴", page: "updates", top: "ページ更新履歴"});
 });
 
 app.get(["/games", "/games.html"], (req, res) => {
-  render(res, "games", { title: "tonkatsu0211のページ", page: "games", top: "ゲームをプレイ"});
+  render(req, res, "games", { title: "tonkatsu0211のページ", page: "games", top: "ゲームをプレイ"});
 });
 
 app.get(["/snow", "/snow.html"], (req, res) => {
-  render(res, "snow", { title: "tonkatsu0211のページ"});
+  render(req, res, "snow", { title: "tonkatsu0211のページ"});
 });
 
 app.get(["/stickman", "/stickman.html"], (req, res) => {
-  render(res, "stickman", { title: "tonkatsu0211のページ"});
+  render(req, res, "stickman", { title: "tonkatsu0211のページ"});
 });
 
 app.get(["/error", "/error.html"], (req, res) => {
-  render(res, "error", { title: "404 Not Found", page: "error"});
+  render(req, res, "error", { title: "404 Not Found", page: "error"});
 });
 
 app.use((req, res) => {

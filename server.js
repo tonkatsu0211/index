2d01e35b80f9604538d93a68a170ceda8e42810c
Glitch (glitch-hello-website) <none>
2025-06-19 10:50:11 +0000

    🚋👘 Checkpoint ./server.js:78855186/346 ./chatHistory.json:78855186/38

--- server.js
+++ server.js
@@ -62,7 +62,16 @@ io.on('connection', (socket) => {
       id: uuidv4(),
       username: socket.data.username,
       message: msg,
-      timestamp: new Date().toLocaleString('ja-JP', { hour12: false }),
+      timestamp: new Intl.DateTimeFormat('ja-JP', {
+        timeZone: 'Asia/Tokyo',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+      }).format(new Date())
     };
     
     console.log("messageData: ", messageData);
